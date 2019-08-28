ca83c23cc898ccec95e6fa265e1f2df292bfb10c
Colin D <colinv120@gmail.com>
2019-08-27 20:43:18 -0600

    inquirer asked for bid or post

--- auction_db.sql
+++ auction_db.sql
@@ -1,12 +1,11 @@
 CREATE DATABASE auction_db;
 
--- Makes it so all of the following code will affect animals_db --
 USE auction_db;
 
 CREATE TABLE items (
     id INTEGER(11) NOT NULL AUTO_INCREMENT,
     item_name TEXT,
     category TEXT,
-    opening_bid TEXT,
+    opening_bid FLOAT,
     PRIMARY KEY (id)
 );
--- greatbay.js
+++ greatbay.js
@@ -14,3 +14,82 @@ var connection = mysql.createConnection({
     password: "saugatuck244!",
     database: "auction_db"
 });
+
+// INQUIRE
+function startApp() {
+    inquirer.prompt([{
+        type: "input",
+        message: "Would you like to [BID] on an item or [POST] an item?",
+        name: "userChoice"
+    }]).then(function (res) {
+        if (res.userChoice === "BID") {
+            selectAll()
+        } else if (res.userChoice === "POST") {
+            postItem()
+        } else {
+            console.log("That's not a choice");
+        }
+    })
+}
+
+function postItem() {
+    inquirer.prompt([{
+        type: "input",
+        message: "What's the name of the item?",
+        name: "itemName"
+    }, {
+        type: "input",
+        message: "What category would you place this item under?",
+        name: "itemCat"
+    }, {
+        type: "number",
+        message: "What is the opening bid on this item?",
+        name: "itemOBid"
+    }]).then(function (res) {
+        createItem(res.itemName, res.itemCat, res.ItemOBid)
+    })
+}
+// DB CONNECTION 
+
+
+function selectAll() {
+    connection.connect(function (err) {
+        if (err) throw err;
+        console.log("connected as id " + connection.threadId);
+        connection.query('SELECT * FROM items', function (err, res) {
+            if (err) throw err;
+
+            console.log(res);
+        })
+
+        connection.end();
+    });
+
+}
+
+function createItem(name, cat, bid) {
+    connection.connect(function (err) {
+        if (err) throw err;
+        console.log("connected as id " + connection.threadId);
+        console.log("Inserting a new Item...\n");
+
+
+        connection.query(
+            "INSERT INTO items SET ?", {
+                item_name: name,
+                category: cat,
+                opening_bid: bid
+            },
+            function (err, res) {
+                if (err) throw err;
+                console.log(res.affectedRows + " product inserted!\n");
+                // Call updateProduct AFTER the INSERT completes
+
+            }
+        );
+
+        connection.end();
+    });
+
+}
+startApp()
